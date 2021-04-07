7039cd356772f86c08bdb62077a3350e51b3ef4a
Leesuhuck <globalmainpcb@gmail.com>
2021-04-07 20:38:42 +0900

    0407

---
+++ idxJs.js
@@ -0,0 +1,19 @@
+alert("Hello world it's Javascript");
+console.log('window.console.log Javascript');
+
+/*
+1. Create 생성
+2. Initlalize 초기화
+3. Use 사용
+
+var 재할당 재선언 둘다 가능하다 (하지만 var가 많을수록 변수선언에 대해 찾기 어렵다.)
+const 재햘당 재선언 둘다 불가능
+let 재할당만 가능
+*/
+
+var a   = 221
+var a   = 177
+const b = a - 5.1
+let c   = 166
+    c   = 'name'
+console.log(a, b, c)
