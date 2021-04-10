a37442857832019826b757c2f4f8b91e9585209d
Leesuhuck <globalmainpcb@gmail.com>
2021-04-10 16:48:34 +0900

    0410

--- idxJs.js
+++ idxJs.js
@@ -9,11 +9,24 @@ console.log('window.console.log Javascript');
 var 재할당 재선언 둘다 가능하다 (하지만 var가 많을수록 변수선언에 대해 찾기 어렵다.)
 const 재햘당 재선언 둘다 불가능
 let 재할당만 가능
+
+[] = 브라켓
+{} = 컬리 브라켓
+Array 선언 const a = []
+Object 선언 const a = {}
 */
 
-var a   = 221
-var a   = 177
-const b = a - 5.1
-let c   = 166
-    c   = 'name'
-console.log(a, b, c)
+var a   = 221;
+var a   = 177;
+const b = a - 5.1;
+let c   = 166,
+    c   = 'name';
+console.log(a, b, c);
+
+const objectA = {
+    name : "Lee",
+    age : "23",
+    sex : "man"
+};
+
+console.log (objectA);
